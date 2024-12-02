3f7da838ece56eee2346b8cd98d65f113e49fe5e
Gianna Read-Skelton <giannamread@gmail.com>
2024-12-02 09:32:20 -0700

    started adding JS countdown clock

---
+++ index.js
@@ -0,0 +1,9 @@
+const date = new Date();
+const christmasDay = new Date("2024-12-25");
+
+// const days = christmasDay.getDate() - date.getDate();
+// const hours = christmasDay.getHours() - date.getHours();
+// const minutes = christmasDay.getMinutes() - date.getMinutes();
+// const seconds = christmasDay.getSeconds() - date.getSeconds();
+
+const countdownDiff = christmasDay - date;
