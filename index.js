36822a663bac067206815117555947b7b703081c
Gianna Read-Skelton <giannamread@gmail.com>
2024-12-02 22:35:24 -0700

    updated Christmas day date

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-// console.log(days);
-// console.log(hours);
-// console.log(minutes);
-// console.log(seconds);
-
 setInterval(function () {
   const date = new Date();
-  const christmasDay = new Date("2024-12-25");
+  const christmasDay = new Date(2024, 11, 25, 0, 0, 0, 0);
 
   const countdownDiff = new Date(christmasDay - date);
 
@@ -13,6 +8,7 @@ setInterval(function () {
   const hours = countdownDiff.getHours();
   const minutes = countdownDiff.getMinutes();
   const seconds = countdownDiff.getSeconds();
+
   numberDays.innerHTML = days;
   numberHours.innerHTML = hours;
   numberMinutes.innerHTML = minutes;
