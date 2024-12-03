74d0077b50628a3a0cb5f836cdc396076eeec0fc
Gianna Read-Skelton <giannamread@gmail.com>
2024-12-02 23:24:46 -0700

    Added UTC to fix countdown clock

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 setInterval(function () {
   const date = new Date();
-  const christmasDay = new Date(2024, 11, 25, 0, 0, 0, 0);
+  const christmasDay = new Date(2024, 11, 25, 0, 0, 0);
 
   const countdownDiff = new Date(christmasDay - date);
 
-  const days = countdownDiff.getDate();
-  const hours = countdownDiff.getHours();
-  const minutes = countdownDiff.getMinutes();
-  const seconds = countdownDiff.getSeconds();
+  const days = countdownDiff.getUTCDate();
+  const hours = countdownDiff.getUTCHours();
+  const minutes = countdownDiff.getUTCMinutes();
+  const seconds = countdownDiff.getUTCSeconds();
 
   numberDays.innerHTML = days;
   numberHours.innerHTML = hours;
