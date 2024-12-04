caa2f8d65dc1889c074c449bbebf0f9fc4bb5599
Gianna Read-Skelton <giannamread@gmail.com>
2024-12-04 15:24:14 -0700

    added button js

--- index.js
+++ index.js
@@ -1,4 +1,36 @@
-setInterval(function () {
+home.addEventListener("click", function () {
+  clearInterval(myInterval);
+  document.body.classList.add("home_button");
+  document.body.classList.remove("pistachio_button");
+  document.body.classList.remove("crack_button");
+  document.body.classList.remove("melt_button");
+});
+
+pistachio.addEventListener("click", function () {
+  clearInterval(myInterval);
+  document.body.classList.add("pistachio_button");
+  document.body.classList.remove("home_button");
+  document.body.classList.remove("crack_button");
+  document.body.classList.remove("melt_button");
+});
+
+crack.addEventListener("click", function () {
+  clearInterval(myInterval);
+  document.body.classList.add("crack_button");
+  document.body.classList.remove("pistachio_button");
+  document.body.classList.remove("home_button");
+  document.body.classList.remove("melt_button");
+});
+
+melt.addEventListener("click", function () {
+  clearInterval(myInterval);
+  document.body.classList.add("melt_button");
+  document.body.classList.remove("pistachio_button");
+  document.body.classList.remove("crack_button");
+  document.body.classList.remove("home_button");
+});
+
+let myInterval = setInterval(function () {
   const date = new Date();
   const christmasDay = new Date(2024, 11, 25, 0, 0, 0);
 
