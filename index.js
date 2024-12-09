aaff00e001e6f33d0351a9dc75dd85871054fe1d
Gianna Read-Skelton <giannamread@gmail.com>
2024-12-09 08:36:59 -0700

    cleaned up redundancy in js code

--- index.js
+++ index.js
@@ -1,19 +1,4 @@
-const date = new Date();
-const christmasDay = new Date(2024, 11, 25, 0, 0, 0);
-
-const countdownDiff = new Date(christmasDay - date);
-
-const days = countdownDiff.getUTCDate();
-const hours = countdownDiff.getUTCHours();
-const minutes = countdownDiff.getUTCMinutes();
-const seconds = countdownDiff.getUTCSeconds();
-
-numberDays.innerHTML = days;
-numberHours.innerHTML = hours;
-numberMinutes.innerHTML = minutes;
-numberSeconds.innerHTML = seconds;
-
-setInterval(function () {
+function christmasCountdown() {
   const date = new Date();
   const christmasDay = new Date(2024, 11, 25, 0, 0, 0);
 
@@ -28,4 +13,8 @@ setInterval(function () {
   numberHours.innerHTML = hours;
   numberMinutes.innerHTML = minutes;
   numberSeconds.innerHTML = seconds;
-}, 1000);
+}
+
+christmasCountdown();
+
+setInterval(christmasCountdown, 1000);
