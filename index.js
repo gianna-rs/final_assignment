e329f435d4942f0a4922e4b807e81ac9a49beea5
Gianna Read-Skelton <giannamread@gmail.com>
2024-12-08 21:33:19 -0700

    adjustment js to have countdown open on load

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+const date = new Date();
+const christmasDay = new Date(2024, 11, 25, 0, 0, 0);
+
+const countdownDiff = new Date(christmasDay - date);
+
+const days = countdownDiff.getUTCDate();
+const hours = countdownDiff.getUTCHours();
+const minutes = countdownDiff.getUTCMinutes();
+const seconds = countdownDiff.getUTCSeconds();
+
+numberDays.innerHTML = days;
+numberHours.innerHTML = hours;
+numberMinutes.innerHTML = minutes;
+numberSeconds.innerHTML = seconds;
+
 setInterval(function () {
   const date = new Date();
   const christmasDay = new Date(2024, 11, 25, 0, 0, 0);
